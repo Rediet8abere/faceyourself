275ce65ef2cfe0ac2932a934e6769b2b013cfb3e
Rediet8abere <redietaberegesesse@gmail.com>
2019-10-15 14:43:11 -0700

    comment page added

--- app.py
+++ app.py
@@ -54,6 +54,11 @@ def login():
     if request.method == 'GET':
         return render_template('login.html', form=form)
 
+@app.route('/comment')
+def comment():
+    """commentpage."""
+    # return "holla"
+    return render_template('comment_form.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
