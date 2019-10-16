7945e2068c4611618849ca0c75906e015e37223c
Rediet8abere <redietaberegesesse@gmail.com>
2019-10-15 20:46:08 -0700

    companey added

--- app.py
+++ app.py
@@ -8,8 +8,9 @@ from user import user
 
 client = MongoClient()
 db = client.faceyourself
-users = db.users
+employees = db.employees
 comments = db.comments
+companies = db.companies
 
 app = Flask(__name__)
 
@@ -26,11 +27,11 @@ def register():
     form = RegistrationForm()
     if request.method == 'POST':
         if form.validate_on_submit():
-            if users.find_one({"username": form.username.data}):
+            if employees.find_one({"username": form.username.data}):
                 flash(f'That account already exists')
                 return redirect(url_for('index'))
             else:
-                current_user = users.insert_one(user(form.username.data, form.password.data, form.email.data).json())
+                current_user = employees.insert_one(user(form.username.data, form.password.data, form.email.data).json())
                 return redirect(url_for('index'))
         else:
             flash(f'Incorrect crednetials')
@@ -41,19 +42,44 @@ def register():
 
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
-    """Takes user to regestration page"""
+    """Enables user to login"""
     form = LoginForm()
     if request.method == 'POST':
         if form.validate_on_submit():
-            if users.find_one({"email": form.email.data}):
-                if (form.email.data == users.find_one({"email": form.email.data})["email"]) and (form.password.data == users.find_one({"email": form.email.data})["password"]):
-                    return redirect(url_for('comment'))
+            if employees.find_one({"email": form.email.data}):
+                if (form.email.data == employees.find_one({"email": form.email.data})["email"]) and (form.password.data == employees.find_one({"email": form.email.data})["password"]):
+                    return redirect(url_for('add_company'))
             else:
                 flash(f'Log in unsuccessful. Please Check password and email', 'danger')
     return render_template('login.html', form=form)
 
     if request.method == 'GET':
         return render_template('login.html', form=form)
+
+@app.route('/add/company', methods = ['GET', 'POST'])
+def add_company():
+    """Add Company."""
+    if request.method == 'POST':
+        company = {
+            'title': request.form.get('title'),
+            'description': request.form.get('description')
+            }
+        companies.insert_one(company)
+        return redirect(url_for('show_companies'))
+
+    if request.method == 'GET':
+        return render_template('company/add_company.html')
+
+@app.route('/companies/show')
+def show_companies():
+    """Show all companies."""
+    return render_template('company/show_companies.html', companies=companies.find())
+
+# @app.route('/companies/<company_id>')
+# def show_companies(company_id):
+#     """Show all companies."""
+#     company = companies.find_one({'_id': ObjectId(company_id)})
+#     return render_template('company/show_companies.html', company=company)
 
 @app.route('/comment', methods = ['GET', 'POST'])
 def comment():
@@ -67,7 +93,12 @@ def comment():
         return redirect(url_for('index'))
 
     if request.method == 'GET':
-        return render_template('comment_form.html')
+        return render_template('partials/comment_form.html')
+
+@app.route('/showcomment')
+def show_comment():
+    """view all comment."""
+    return render_template('partials/show_comments.html', comments = comments.find())
 
 if __name__ == '__main__':
     app.run(debug=True)
